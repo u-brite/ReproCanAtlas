6335905e98a93ef4d066f39454e87a02409e2d15
page22emily <page22emily@gmail.com>
2022-08-06 18:53:11 -0500

    Edits for App 7pm Aug 6

--- app_1/app.R
+++ app_1/app.R
@@ -4,31 +4,33 @@ library(maftools)
 endometrial_maf <- tcgaLoad(study= "UCEC")
 
 
-
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
   #Sidebar panel for inputs ----
     sidebarPanel( 
-      checkboxGroupInput("Data_Types", label = h3("Data Types to View"), choices = c("WXS", "RNAseq", "scRNAseq", "Cell Line"), selected = NULL)),
+      checkboxGroupInput("Data", label = h3("Data Types to View"), choices = c("WXS", "RNAseq", "scRNAseq", "Cell Line"), selected = NULL)),
+  
+      textInput("Gene of Interest","Gene of Interest for RNAseq"),
+  
+  
   # App title ----
   titlePanel("ReproCanAtlas"),
     
     # Main panel for displaying outputs ----
   
   mainPanel(
-  splitLayout(plotOutput(outputId = "summary"),  
+   plotOutput(outputId = "summary"),  
     plotOutput(outputId = "oncoplot"))
   )
-) 
+
 
 
 # Define server logic required to ----
 server <- function(input, output) {
-
-   output$summary <- renderPlot(plotmafSummary(maf = endometrial_maf, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE))   
+    
+  output$summary <- renderPlot(plotmafSummary(maf = endometrial_maf, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE))   
 
   output$oncoplot <- renderPlot(oncoplot(maf = endometrial_maf, top = 10))
-
   } 
   
 
