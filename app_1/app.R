b73e5394c49ab9399c58291e9097f7a62dc871c3
page22emily <page22emily@gmail.com>
2022-08-06 16:53:00 -0500

    5 pm updates

--- app_1/app.R
+++ app_1/app.R
@@ -7,26 +7,29 @@ endometrial_maf <- tcgaLoad(study= "UCEC")
 
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
-  
+  #Sidebar panel for inputs ----
+    sidebarPanel( 
+      checkboxGroupInput("Data_Types", label = h3("Data Types to View"), choices = c("WXS", "RNAseq", "scRNAseq", "Cell Line"), selected = NULL)),
   # App title ----
   titlePanel("ReproCanAtlas"),
     
     # Main panel for displaying outputs ----
-    mainPanel("Genomic Analysis Selection"), 
-      
-      # Output:
-  splitLayout(plotOutput("plot1"), plotOutput("plot2"))
   
-  
-)
+  mainPanel(
+  splitLayout(plotOutput(outputId = "summary"),  
+    plotOutput(outputId = "oncoplot"))
+  )
+) 
 
 
 # Define server logic required to ----
 server <- function(input, output) {
-  output$plot1 <- renderPlot(plotmafSummary(maf = endometrial_maf, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE),
-  output$plot2 <- renderPlot(oncoplot(maf = endometrial_maf, top = 10))
-                             )
-     }
+
+   output$summary <- renderPlot(plotmafSummary(maf = endometrial_maf, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE))   
+
+  output$oncoplot <- renderPlot(oncoplot(maf = endometrial_maf, top = 10))
+
+  } 
   
 
 shinyApp(ui = ui, server = server)
