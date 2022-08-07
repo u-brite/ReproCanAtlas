5d704ca137f8a8a5ca54ef6f0b404069bfc134be
gitmee <fion.sun@gmail.com>
2022-08-06 21:28:31 -0700

    shiny app

---
+++ app_1/install.R
@@ -0,0 +1,12 @@
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("recount3")
+BiocManager::install("tximport")
+BiocManager::install("GenomicFeatures")
+BiocManager::install("TxDb.Hsapiens.UCSC.hg19.knownGene")
+BiocManager::install("vsn")
+BiocManager::install("DESeq2")
+BiocManager::install("maftools")
+BiocManager::install("apeglm")
+install.packages("tidyverse")
